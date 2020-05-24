52d5fdd6dc4f37032ba3e8450be0b058fccfc4fd
Kunwar Shaanjeet Singh Grover <groverkss@gmail.com>
2020-05-24 15:52:01 +0530

    Added table classes in app.py

--- app.py
+++ app.py
@@ -9,6 +9,34 @@ _INDEX = 0
 _SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = _SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/order.db'
+db = SQLAlchemy(app)
+
+
+class CMaggi(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    count = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, id, count):
+        self.id = id
+        self.count = count
+
+
+class PMaggi(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    count = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, id, count):
+        self.id = id
+        self.count = count
+
+
+class MDosa(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    count = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, id, count):
+        self.id = id
+        self.count = count
 
 
 @app.route('/', methods=('GET', 'POST'))
@@ -28,4 +56,6 @@ def order(id):
 
 
 if __name__ == '__main__':
+    db.create_all()
+    db.session.commit()
     app.run(debug=True)
