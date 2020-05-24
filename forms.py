c858f47368f5e8281af4b366e196b550e94903e0
poojadesur <poojadesur@gmail.com>
2020-05-24 11:45:57 +0530

    added config and forms.py

---
+++ forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm,
+from wtforms import StringField, TextField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class OrderForm(FlaskForm):
+    """Contact form."""
+    cheeseMaggie = StringField('cheeseMaggie', [
+        DataRequired()])
+    plainMaggie = StringField('plainMaggie', [
+        DataRequired()])
+    masalaDosa = TextField('masalaDosa', [
+        DataRequired()])
+ 
+    submit = SubmitField('Submit')
